2493873a4aad0b3681077aa4bcffb166f54ac416
4aenyeweddien <manackov.vitek@gmail.com>
2026-03-01 14:24:05 +0300

    создание CRUD для пользователей

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from pathlib import Path
 
 from django.conf.global_settings import MEDIA_URL
@@ -29,6 +30,7 @@ INSTALLED_APPS = [
     "materials",
     "django_filters",
     "django_extensions",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -61,7 +63,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 REST_FRAMEWORK = {
-    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"]
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
 
 DATABASES = {
@@ -109,3 +117,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from users.apps import UsersConfig
 from users.views import PaymentViewSet, UserViewSet
@@ -12,4 +14,8 @@ router.register("payments", PaymentViewSet)  # /users/payments/
 
 urlpatterns = [
     path("", include(router.urls)),
+    path(
+        "login/",TokenObtainPairView.as_view(),name="login/"),
+    path(
+        "token/refresh/",TokenRefreshView.as_view(),name="token_refresh"),
 ]
